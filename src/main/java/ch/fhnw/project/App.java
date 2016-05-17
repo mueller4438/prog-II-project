a0ea8e9e7afe79b108a68774dde55b4606cdf044
Balz Müller <a@b.ch>
2016-05-17 14:11:42 +0200

    LineReader und TabReader geschrieben Hinzufügen von Tests, (Durchgefallene Tests sollten noch im Code angepasst werden, (Fehlermeldung ausgeben)). Hat aber 2 Priorität. Filechooser mit beschränkter auswahl(lin und txt) hinzugefügt. richtige auswahl der Methoden durch Überprüfung der Endungen Erster Scatterplot ohne funktionen

--- src/main/java/ch/fhnw/project/App.java
+++ src/main/java/ch/fhnw/project/App.java
@@ -1,11 +1,16 @@
 package ch.fhnw.project;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.function.Supplier;
 
 public final class App extends Application {
@@ -13,24 +18,26 @@ public final class App extends Application {
         launch(args);
     }
 
+    private String  firstvariable,secondvariable;
+    private Double[] dataFirstvariable,dataSecondvariable;
+
     @Override
     public void start(Stage primarystage) {
 
         //select a line or tab oriented file
-        FileChooser fc=new FileChooser();
+        FileChooser fc = new FileChooser();
         fc.setTitle("Open Resource File");
         fc.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("LineOriented", "*.lin"),
                 new FileChooser.ExtensionFilter("TabOriented", "*.txt")
         );
-        //read Data
-        File selectedFile=fc.showOpenDialog(primarystage);
+        //read and parse Data
+        File selectedFile = fc.showOpenDialog(primarystage);
         try {
-            if(selectedFile.getName().endsWith(".txt")){
+            if (selectedFile.getName().endsWith(".txt")) {
                 Data mydata = new TabReader().parseContents(selectedFile);
                 System.out.print(mydata);
-            }
-            else if(selectedFile.getName().endsWith(".lin")){
+            } else if (selectedFile.getName().endsWith(".lin")) {
                 Data mydata = new LineReader().parseContents(selectedFile);
                 System.out.print(mydata);
             }
@@ -39,10 +46,17 @@ public final class App extends Application {
             showErrorMessage(e.getMessage());
 
         }
+        // Layout
+        StackPane pane = new StackPane();
+
+        primarystage.setTitle("Datenvisualisierung");
+        Scene scene = new Scene(pane);
+        primarystage.setScene(scene);
+        primarystage.show();
     }
 
 
-    private void showErrorMessage(String msg){
+    private void showErrorMessage(String msg) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("An Error occurred: ");
@@ -51,8 +65,38 @@ public final class App extends Application {
         alert.showAndWait();
     }
 
+    public void XYChart(Data data) {
+        //get variable names
+        Set<String> vars = data.getVariableNames();
+        Iterator<String> iter = vars.iterator();
+        firstvariable = iter.next();
+        if(iter.hasNext()){
+            secondvariable=iter.next();}
+        else firstvariable=secondvariable;
+        //get values for variable
+        dataForVariable(data);
+        XYChart.Series series1 = new XYChart.Series<>();
+        for(int i=0;i<dataFirstvariable.length;i++)
+        series1.getData().add(new XYChart.Data(dataFirstvariable[i],dataSecondvariable[i]));
 
 
 
+
+        }
+
+    private void dataForVariable(Data data) {
+        dataFirstvariable= data.getDataForVariable(firstvariable);
+        dataSecondvariable=data.getDataForVariable(secondvariable);
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
